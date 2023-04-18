65d86f9bd3329d86e09be00cf8fa628faccc002c
Carles Fernández Tena <c.fernandeztena@oxolo.com>
2023-04-18 18:58:51 +0200

    [fix] mp3_from_youtube via dynlink to yt-dlp repo bin

--- mp3_from_youtube.sh
+++ mp3_from_youtube.sh
@@ -1,5 +1,5 @@
 if [[ -z "$1" ]]; then
     echo "Usage: $0 <youtube url>"
 else
-    youtube-dl -i --extract-audio --audio-format mp3 $1
+    yt-dlp_linux -i --extract-audio --audio-format mp3 $1
 fi
