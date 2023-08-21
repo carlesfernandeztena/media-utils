3452e36583345ffb6ad65c122a3a170fed57c60e
Carles Fernández Tena <c.fernandeztena@oxolo.com>
2023-08-21 19:59:52 +0200

    [refine] comments video face crop

--- vid_face_crop.sh
+++ vid_face_crop.sh
@@ -5,13 +5,15 @@ if [ "$#" -ne 1 ]; then
 fi
 VID=$1
 
+# compute num frames
 num_frames=$(ffprobe -v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0 "$VID")
-
-ffmpeg -y -hide_banner -v warning -i "$VID"  -vf "select=eq(n\,$((num_frames/2)))" -frames:v 1 _tmp.png
+# extract temp frame
+ffmpeg -y -hide_banner -v error -i "$VID"  -vf "select=eq(n\,$((num_frames/2)))" -frames:v 1 _tmp.png
+# detect bbox
 bbox=( $(face_detect.py _tmp.png) )
 X=${bbox[0]}
 Y=${bbox[1]}
 W=${bbox[2]}
 rm _tmp.png
-echo "Found face: x=${X} y=${Y} w=${W}"
+echo "    Found face: x=${X} y=${Y} w=${W}"
 ffmpeg -y -hide_banner -v warning -i "$VID" -filter:v "crop=$W:$W:$X:$Y" -c:a copy "facecrop_$VID"
